7ffa9b1d54991be89bda089fd3a84f4e943d142e
Blitheness <andy@silver.sucs.swan.ac.uk>
2023-02-28 02:01:07 +0000

    Add dockerfile and build script

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+FROM node:14.21.1-alpine3.15
+
+ENTRYPOINT ["/bin/ash"]
+
+WORKDIR "/root"
+
+RUN cat /etc/profile > /root/.profile
+
+RUN apk add --no-cache \
+		bash coreutils curl nodejs unzip yarn \
+	&& apk add --no-cache \
+		icu-libs \
+		php7 \
+		php7-ctype php7-curl \
+		php7-dom \
+		php7-fileinfo \
+		php7-iconv php7-intl \
+		php7-json \
+		php7-mbstring \
+		php7-openssl \
+		php7-pdo php7-phar \
+		php7-simplexml \
+		php7-tokenizer \
+		php7-xml php7-xmlwriter \
+	&& curl -sS https://getcomposer.org/installer \
+		| php -- --install-dir=/usr/local/bin --filename=composer --version=1.10.26
+
+COPY get-and-build-vanilla.sh /usr/local/bin/get-and-build-vanilla
+RUN chmod +x /usr/local/bin/get-and-build-vanilla
+
+ENTRYPOINT ["get-and-build-vanilla"]
---
+++ get-and-build-vanilla.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+rm -rf /root/output/*
+curl -s https://api.github.com/repos/vanilla/vanilla/releases/latest \
+		| grep "zipball_url.*," \
+		| cut -d : -f 2,3 \
+		| tr -d '", ' \
+		| xargs wget
+unzip -q $(ls .|head -n 1)
+cd $(ls|sort -r|head -n 1)
+composer require --no-update moontoast/math
+composer install --prefer-dist
+
+cp -r addons /root/output
+cp -r applications /root/output
+cp -r cache /root/output
+cp -r conf /root/output
+cp -r dist /root/output
+cp -r js /root/output
+cp -r library /root/output
+cp -r locales /root/output
+cp -r plugins /root/output
+cp -r resources /root/output
+cp -r themes /root/output
+cp -r uploads /root/output
+cp -r vendor /root/output
+cp .htaccess.dist /root/output
+cp LICENSE /root/output
+cp README.md /root/output
+cp bootstrap.php /root/output
+cp environment.php /root/output
+cp index.php /root/output
+cp preload.php /root/output
+cp version.json /root/output
